604ec8a2d06e260545e4b989131e352b6496c5b4
刘祖宽 <liuzukuan@liuzukuandeMacBook-Pro.local>
2016-10-12 23:20:30 +0800

    地址修改

--- public/html/blog/js/controllers.js
+++ public/html/blog/js/controllers.js
@@ -173,7 +173,7 @@ blogControllers.controller('UpdateBlogCtrl', ['$scope', 'blogS', '$ngBootbox', '
 
         $scope.getblog();
         
-        $rootScope.socket = io('http://115.159.52.72:8000');
+        $rootScope.socket = io('http://115.159.146.35:8000');
         $rootScope.socket.on('contentS', function (data) {
             console.log('--------->')
             console.log(data);
